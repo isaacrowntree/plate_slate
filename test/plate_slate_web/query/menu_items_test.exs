3c0051a11504f124281c09b235bcb1ca3a73cd30
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-09-06 11:47:11 -0700

    Testing variables

--- test/plate_slate_web/query/menu_items_test.exs
+++ test/plate_slate_web/query/menu_items_test.exs
@@ -6,39 +6,37 @@ defmodule PlateSlateWeb.Query.MenuItemsTest do
     :ok
   end
 
-  test "list menu items without filter with GET" do
-    query = """
-    {
-      menuItems { name }
-    }
-    """
+  @query """
+  query ($term: String) {
+    menuItems(matching:$term) { name }
+  }
+  """
 
-    conn = get build_conn(), "/", query: query
+  test "list menu items with filter and variables GET" do
+    variables = %{"term" => "Rue"}
+
+    conn = get build_conn(), "/", query: @query, variables: variables
 
     assert %{"data" => %{"menuItems" => [item | _] }} = json_response(conn, 200)
     assert item ==  %{"name" => "Rueben"}
   end
 
-  test "list menu items without filter with POST" do
-    query = """
-    {
-      menuItems { name }
-    }
-    """
+  test "list menu items with filter, variables, POST" do
+    variables = %{"term" => "Rue"}
 
     conn =
       build_conn()
       |> Plug.Conn.put_req_header("content-type", "application/json")
-      |> post("/", %{'query' => query})
+      |> post("/", %{'query' => @query, "variables" => variables})
 
-    assert %{"data" => %{"menuItems" => [item | _] }} = json_response(conn, 200)
+    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
     assert item == %{"name" => "Rueben"}
   end
 
-  test "list menu items with filter" do
+  test "list menu items without filter" do
     query = """
     {
-      menuItems(matching: \"Rue\") {
+      menuItems {
         name
       }
     }
@@ -49,7 +47,7 @@ defmodule PlateSlateWeb.Query.MenuItemsTest do
       |> Plug.Conn.put_req_header("content-type", "application/json")
       |> post("/", %{'query' => query})
 
-    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
+    assert %{"data" => %{"menuItems" => [item | _] }} = json_response(conn, 200)
     assert item == %{"name" => "Rueben"}
   end
 end
