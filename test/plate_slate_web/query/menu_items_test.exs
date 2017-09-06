eb83706fc67f2e99391c16e351c2dd9f44cc8737
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-09-06 11:27:48 -0700

    Third test with matching

--- test/plate_slate_web/query/menu_items_test.exs
+++ test/plate_slate_web/query/menu_items_test.exs
@@ -32,6 +32,24 @@ defmodule PlateSlateWeb.Query.MenuItemsTest do
       |> post("/", %{'query' => query})
 
     assert %{"data" => %{"menuItems" => [item | _] }} = json_response(conn, 200)
-    assert item ==  %{"name" => "Rueben"}
+    assert item == %{"name" => "Rueben"}
+  end
+
+  test "list menu items with filter" do
+    query = """
+    {
+      menuItems(matching: \"Rue\") {
+        name
+      }
+    }
+    """
+
+    conn =
+      build_conn()
+      |> Plug.Conn.put_req_header("content-type", "application/json")
+      |> post("/", %{'query' => query})
+
+    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
+    assert item == %{"name" => "Rueben"}
   end
 end
